13fad4fa3940172adf8a49b54b58e258546f4234
SaiKumar Pallagani <saikumar.pallagani@hedgehoglab.com>
2018-10-28 12:43:07 +0530

    OnBoarding completed

---
+++ Happy Roads/OnBoarding/LandingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LandingViewController.swift
+//  Happy Roads
+//
+//  Created by Saikumar on 28/10/18.
+//  Copyright © 2018 Saikumar. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
