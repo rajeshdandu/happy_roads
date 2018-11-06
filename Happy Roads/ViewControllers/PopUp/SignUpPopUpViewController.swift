a035836e42982fb0cee49bb8c8d287e8c4b5cc4d
brn.developers <brn.developers@Achiever-48.local>
2018-11-06 18:36:46 +0530

    LOGINVIEW HAS BEEN COMPLETED

--- Happy Roads/ViewControllers/PopUp/SignUpPopUpViewController.swift
+++ Happy Roads/ViewControllers/PopUp/SignUpPopUpViewController.swift
@@ -11,23 +11,48 @@ import JVFloatLabeledTextField
 
 class SignUpPopUpViewController: UIViewController {
 
+    
+    @IBOutlet weak var loginView: UIView!
+    
+    
+    
     @IBOutlet weak var backgroundView: UIView!
     
     
     @IBOutlet weak var nameTextField: JVFloatLabeledTextField!
     
+    @IBOutlet weak var mobileTextField: JVFloatLabeledTextField!
+    
+    @IBOutlet weak var emailTextField: JVFloatLabeledTextField!
+    
+    @IBOutlet weak var passwordTextField: JVFloatLabeledTextField!
+    
+    @IBOutlet weak var loginEmailTextField: JVFloatLabeledTextField!
+    
+    @IBOutlet weak var loginPasswordTextField: JVFloatLabeledTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loginView.isHidden=true
+        designUI()
+        onPopBackgroundBlur()
+        // Do any additional setup after loading the view.
+    }
+    func designUI()
+    {
+        nameTextField.useUnderlineColor()
+        mobileTextField.useUnderlineColor()
+        emailTextField.useUnderlineColor()
+        passwordTextField.useUnderlineColor()
+        loginEmailTextField.useUnderlineColor()
+        loginPasswordTextField.useUnderlineColor()
+    }
+    func onPopBackgroundBlur()
+    {
         let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.dark)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = view.bounds
         blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         backgroundView.addSubview(blurEffectView)
-        
-        nameTextField.useUnderlineColor()
-        
-        // Do any additional setup after loading the view.
     }
     
 
@@ -41,6 +66,17 @@ class SignUpPopUpViewController: UIViewController {
     }
     */
     
+    @IBAction func loginBtnAction(_ sender: Any) {
+        loginView.isHidden=false
+    }
+    
+    @IBAction func signUpBtnAction(_ sender: Any) {
+        loginView.isHidden=true
+    }
+    
+    @IBAction func signUpInLoginView(_ sender: Any) {
+        loginView.isHidden=true
+    }
     
     @IBAction func closeBtnAction(_ sender: UIButton) {
         
